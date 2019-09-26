7147265a85fafe83bcdf9ff55eb161e64bd05003
manuhackzzz <manojgoswami131@gmail.com>
2019-09-26 20:36:38 +0530

    updated on 26-Sep-2019

---
+++ .sfdx/typings/lwc/apex/AccountWeatherCompController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/AccountWeatherCompController.getDetails" {
+  export default function getDetails(param: {accId: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/sound1" {
+    var sound1: string;
+    export default sound1;
+}
